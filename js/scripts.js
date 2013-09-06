7f580acec1da15ff892803d656b6f3413c4076ec
pragunbhutani <pragun06@gmail.com>
2013-09-06 15:10:18 +0530

    background and color switching implemented

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+	var $win = $(window),
+    w = 0,h = 0,
+    rgb = [],
+    getWidth = function() {
+        w = $win.width();
+        h = $win.height();
+    };
+
+	$win.resize(getWidth).mousemove(function(e) {
+
+		rgb = [
+			Math.round(e.pageX/w * 255),
+			Math.round(e.pageY/h * 255),
+			150
+		];
+
+		$('#background-container').css('background','rgb('+rgb.join(',')+')');
+
+	}).resize();
+
+});
